f80da77ffea4da261ea67d1415a3467d49a0ff64
ivanhuerta20 <ivanhuerta20@gmail.com>
2021-10-17 15:28:28 -0500

    Removed Center widget from amount

--- lib/egg/amount.dart
+++ lib/egg/amount.dart
@@ -14,42 +14,44 @@ class Amount extends StatefulWidget {
 class _AmountState extends State<Amount> {
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Positioned(
-        left: 0,
-        width: MediaQuery.of(context).size.width * 0.893,
-        top: 0,
-        height: MediaQuery.of(context).size.height * 0.193,
-        child: Container(
-          width: MediaQuery.of(context).size.width * 335.000,
-          height: MediaQuery.of(context).size.height * 54.000,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(0)),
+    return Container(
+      child: Stack(children: [
+        Positioned(
+          left: 0,
+          width: MediaQuery.of(context).size.width * 0.893,
+          top: 0,
+          height: MediaQuery.of(context).size.height * 0.193,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 335.000,
+            height: MediaQuery.of(context).size.height * 54.000,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(0)),
+            ),
           ),
         ),
-      ),
-      Positioned(
-        left: 0,
-        width: MediaQuery.of(context).size.width * 0.893,
-        top: 0,
-        height: MediaQuery.of(context).size.height * 0.193,
-        child: Center(
-            child: Container(
-                width: 335.0,
-                child: Container(
-                    child: AutoSizeText(
-                  "\$ ${context.watch<WalletBalanceCubit>().state.balance.toString()}",
-                  style: TextStyle(
-                    fontFamily: 'Prompt',
-                    fontSize: 36.0,
-                    fontWeight: FontWeight.w700,
-                    fontStyle: FontStyle.normal,
-                    letterSpacing: 0.0,
-                    color: Colors.white,
-                  ),
-                  textAlign: TextAlign.center,
-                )))),
-      ),
-    ]);
+        Positioned(
+          left: 0,
+          width: MediaQuery.of(context).size.width * 0.893,
+          top: 0,
+          height: MediaQuery.of(context).size.height * 0.193,
+          child: Container(
+              width: 335.0,
+              height: 50,
+              child: Container(
+                  child: AutoSizeText(
+                "\$ ${context.watch<WalletBalanceCubit>().state.balance.toString()}",
+                style: TextStyle(
+                  fontFamily: 'Prompt',
+                  fontSize: 36.0,
+                  fontWeight: FontWeight.w700,
+                  fontStyle: FontStyle.normal,
+                  letterSpacing: 0.0,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ))),
+        ),
+      ]),
+    );
   }
 }
