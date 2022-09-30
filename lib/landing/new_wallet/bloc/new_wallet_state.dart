0365bdb30f05dbed984c17cf91f91763d109ffbb
Ivan <42812006+ivan-015@users.noreply.github.com>
2022-09-30 14:51:11 -0600

    Display recovery phrases Add ability to copy recovery phrases

--- lib/landing/new_wallet/bloc/new_wallet_state.dart
+++ lib/landing/new_wallet/bloc/new_wallet_state.dart
@@ -2,16 +2,28 @@ part of 'new_wallet_bloc.dart';
 
 class NewWalletState {
   final NewWalletStep currentStep;
+  final NewWalletStatus recoveryPhraseStatus;
+  final NewWalletStatus recoveryPhraseCopied;
+  final List<String> recoveryWords;
 
   const NewWalletState({
     this.currentStep = NewWalletStep.copyPhrase,
+    this.recoveryPhraseStatus = NewWalletStatus.initial,
+    this.recoveryPhraseCopied = NewWalletStatus.initial,
+    this.recoveryWords = const [],
   });
 
   NewWalletState copyWith({
     NewWalletStep? currentStep,
+    NewWalletStatus? recoveryPhraseStatus,
+    NewWalletStatus? recoveryPhraseCopied,
+    List<String>? recoveryWords,
   }) {
     return NewWalletState(
       currentStep: currentStep ?? this.currentStep,
+      recoveryPhraseStatus: recoveryPhraseStatus ?? this.recoveryPhraseStatus,
+      recoveryWords: recoveryWords ?? this.recoveryWords,
+      recoveryPhraseCopied: recoveryPhraseCopied ?? this.recoveryPhraseCopied,
     );
   }
 }
@@ -19,4 +31,12 @@ class NewWalletState {
 enum NewWalletStep {
   copyPhrase,
   verifyRecoveryPhrase,
+}
+
+/// Indicates the loading status of the screen
+enum NewWalletStatus {
+  initial,
+  loading,
+  loaded,
+  error,
 }
--- packages/genius_api/lib/genius_api.dart
+++ packages/genius_api/lib/genius_api.dart
@@ -3,6 +3,6 @@
 /// More dartdocs go here.
 library genius_api;
 
-export 'src/genius_api_base.dart';
+export 'src/genius_api.dart';
 
 // TODO: Export any libraries intended for clients of this package.
