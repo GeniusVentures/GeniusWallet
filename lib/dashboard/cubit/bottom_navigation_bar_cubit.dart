05f97b6bfb3b1dc123f07e463f562ec1eef1f9b4
Ivan <42812006+ivan-015@users.noreply.github.com>
2022-10-12 18:25:18 -0600

    Update BottomNavigationBar Cubit

--- lib/dashboard/cubit/bottom_navigation_bar_cubit.dart
+++ lib/dashboard/cubit/bottom_navigation_bar_cubit.dart
@@ -3,10 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 part 'bottom_navigation_bar_state.dart';
 
 class BottomNavigationBarCubit extends Cubit<BottomNavigationBarState> {
-  BottomNavigationBarCubit()
-      : super(const BottomNavigationBarState(
-          navbarItem: NavbarItem.dashboard,
-        ));
+  BottomNavigationBarCubit({NavbarItem? initialItem})
+      : super(BottomNavigationBarState(
+            navbarItem: initialItem ?? NavbarItem.dashboard));
 
   void tabTapped(int index) {
     emit(state.copyWith(navbarItem: NavbarItem.values[index]));
