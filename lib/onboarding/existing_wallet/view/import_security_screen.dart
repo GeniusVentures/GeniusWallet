227c2d0788b018f069f5c4df9220502c4953bb81
Ivan <42812006+ivan-015@users.noreply.github.com>
2022-10-28 12:54:10 -0600

    WIP: Add Transaction cards to `Transactions` tab
    Currently, the cards display placeholder information.

--- lib/onboarding/existing_wallet/view/import_security_screen.dart
+++ lib/onboarding/existing_wallet/view/import_security_screen.dart
@@ -40,121 +40,133 @@ class ImportSecurityScreen extends StatelessWidget {
     final walletNameController =
         TextEditingController(text: '$walletType Wallet');
 
+    final formKey = GlobalKey<FormState>();
+
     return Scaffold(
       body: DefaultTabController(
         length: 4,
-        child: AppScreenWithHeader(
-          title: 'Import $walletType Wallet',
-          subtitle: '',
-          bodyWidgets: [
-            SizedBox(
-              height: 20,
-              width: MediaQuery.of(context).size.width * 0.8,
-              child: const Text('Name'),
-            ),
-            const SizedBox(height: 10),
-            SizedBox(
+        child: Form(
+          key: formKey,
+          child: AppScreenWithHeader(
+            title: 'Import $walletType Wallet',
+            subtitle: '',
+            bodyWidgets: [
+              SizedBox(
+                height: 20,
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: const Text('Name'),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 50,
+                width: MediaQuery.of(context).size.width * 0.8,
+                child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    return TextEntryFieldWidget(
+                      logic: TextFormFieldLogic(
+                        context,
+                        controller: walletNameController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Please enter a wallet name';
+                          }
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 50,
+                child: AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  bottom: const TabBar(
+                    indicatorColor: Colors.white,
+                    tabs: [
+                      Tab(text: 'Phrase'),
+                      Tab(text: 'Keystore'),
+                      Tab(text: 'Private Key'),
+                      Tab(text: 'Address'),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: 400,
+                child: TabBarView(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 25),
+                      child: PhraseTabView(
+                        controller: tabControllers['phrase']!['pasteField']!,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: KeystoreTabView(
+                        passwordController:
+                            tabControllers['keystore']!['passwordField']!,
+                        pasteFieldController:
+                            tabControllers['keystore']!['pasteField']!,
+                      ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: PrivateKeyTabView(
+                          controller:
+                              tabControllers['privatekey']!['pasteField']!,
+                        )),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: AddressTabView(
+                        controller: tabControllers['address']!['pasteField']!,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+            footer: SizedBox(
               height: 50,
               width: MediaQuery.of(context).size.width * 0.8,
               child: LayoutBuilder(
                 builder: (BuildContext context, BoxConstraints constraints) {
-                  return TextEntryFieldWidget(
-                    logic: TextFormFieldLogic(
-                      context,
-                      controller: walletNameController,
+                  return MaterialButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        /// Get currently selected tab
+                        final selectedIndex =
+                            DefaultTabController.of(context)?.index ?? 0;
+
+                        /// Get current entry to access [TextEditingControllers]
+                        final selectedEntry =
+                            tabControllers.entries.toList()[selectedIndex];
+
+                        /// Send event with currently selected tab information
+                        context.read<ExistingWalletBloc>().add(
+                              WalletSecurityEntered(
+                                walletName: walletNameController.text,
+                                walletType: walletType,
+                                securityType: selectedEntry.key,
+                                pasteFieldText:
+                                    selectedEntry.value['pasteField']!.text,
+                                password:
+                                    selectedEntry.value['passwordField']?.text,
+                              ),
+                            );
+                      }
+                    },
+                    child: IsactiveTrue(
+                      constraints,
+                      ovrContinue: 'Import',
                     ),
                   );
                 },
               ),
-            ),
-            const SizedBox(height: 20),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: 50,
-              child: AppBar(
-                backgroundColor: Colors.transparent,
-                elevation: 0,
-                bottom: const TabBar(
-                  indicatorColor: Colors.white,
-                  tabs: [
-                    Tab(text: 'Phrase'),
-                    Tab(text: 'Keystore'),
-                    Tab(text: 'Private Key'),
-                    Tab(text: 'Address'),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 0.8,
-              height: 400,
-              child: TabBarView(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 25),
-                    child: PhraseTabView(
-                      controller: tabControllers['phrase']!['pasteField']!,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10),
-                    child: KeystoreTabView(
-                      passwordController:
-                          tabControllers['keystore']!['passwordField']!,
-                      pasteFieldController:
-                          tabControllers['keystore']!['pasteField']!,
-                    ),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: PrivateKeyTabView(
-                        controller:
-                            tabControllers['privatekey']!['pasteField']!,
-                      )),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 10),
-                    child: AddressTabView(
-                      controller: tabControllers['address']!['pasteField']!,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-          footer: SizedBox(
-            height: 50,
-            width: MediaQuery.of(context).size.width * 0.8,
-            child: LayoutBuilder(
-              builder: (BuildContext context, BoxConstraints constraints) {
-                return MaterialButton(
-                  onPressed: () {
-                    /// Get currently selected tab
-                    final selectedIndex =
-                        DefaultTabController.of(context)?.index ?? 0;
-
-                    /// Get current entry to access [TextEditingControllers]
-                    final selectedEntry =
-                        tabControllers.entries.toList()[selectedIndex];
-
-                    /// Send event with currently selected tab information
-                    context.read<ExistingWalletBloc>().add(
-                          WalletSecurityEntered(
-                            walletName: walletNameController.text,
-                            walletType: walletType,
-                            securityType: selectedEntry.key,
-                            pasteFieldText:
-                                selectedEntry.value['pasteField']!.text,
-                            password:
-                                selectedEntry.value['passwordField']?.text,
-                          ),
-                        );
-                  },
-                  child: IsactiveTrue(
-                    constraints,
-                    ovrContinue: 'Import',
-                  ),
-                );
-              },
             ),
           ),
         ),
