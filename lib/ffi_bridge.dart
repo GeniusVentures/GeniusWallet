cde64e0082e1ccac0227b3ff64c6f7caec453812
david <davidnetizen1903@gmail.com>
2023-01-02 18:05:50 +0900

    update bridge class

--- lib/ffi_bridge.dart
+++ lib/ffi_bridge.dart
@@ -3,9 +3,6 @@ import 'dart:io';
 
 typedef TemperatureFunction = Double Function();
 typedef TemperatureFunctionDart = double Function();
-// TODO: Add new typedef declarations here
-
-// TODO: Add ThreeDayForecast here
 
 class FFIBridge {
   late TemperatureFunctionDart _getTemperature;
