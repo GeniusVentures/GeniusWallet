369c2d69cb8c7a14a4bfb25e7a691f23c0bb97dc
Ivan <42812006+ivan-015@users.noreply.github.com>
2023-01-02 18:11:32 -0700

    WIP: Added skeleton for David to add FFI code

--- lib/app/bloc/app_state.dart
+++ lib/app/bloc/app_state.dart
@@ -8,27 +8,43 @@ class AppState extends Equatable {
 
   final AppStatus subscribeToWalletStatus;
 
-  const AppState({
-    this.wallets = const [],
-    this.subscribeToWalletStatus = AppStatus.initial,
-    this.transactions = const [],
-  });
+  ///TODO: Change these to the proper types and names if needed
+  final String ffiInformation;
+
+  final AppStatus ffiStatus;
+
+  const AppState(
+      {this.wallets = const [],
+      this.subscribeToWalletStatus = AppStatus.initial,
+      this.ffiStatus = AppStatus.initial,
+      this.transactions = const [],
+      this.ffiInformation = ''});
 
   AppState copyWith({
     List<Wallet>? wallets,
     List<Transaction>? transactions,
     AppStatus? subscribeToWalletStatus,
+    AppStatus? ffiStatus,
+    String? information,
   }) {
     return AppState(
       wallets: wallets ?? this.wallets,
       subscribeToWalletStatus:
           subscribeToWalletStatus ?? this.subscribeToWalletStatus,
       transactions: transactions ?? this.transactions,
+      ffiInformation: information ?? this.ffiInformation,
+      ffiStatus: ffiStatus ?? this.ffiStatus,
     );
   }
 
   @override
-  List<Object?> get props => [wallets, transactions, subscribeToWalletStatus];
+  List<Object?> get props => [
+        wallets,
+        transactions,
+        subscribeToWalletStatus,
+        ffiStatus,
+        ffiInformation,
+      ];
 }
 
 enum AppStatus {
--- lib/onboarding/view/wallet_creation_screen.dart
+++ lib/onboarding/view/wallet_creation_screen.dart
@@ -1,6 +1,8 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genius_wallet/app/bloc/app_bloc.dart';
 import 'package:genius_wallet/app/widgets/app_screen_view.dart';
 import 'package:genius_wallet/widgets/components/wallet_button/type_create.g.dart';
 import 'package:genius_wallet/widgets/components/wallet_button/type_existing.g.dart';
@@ -11,58 +13,75 @@ class LandingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //TODO: Can make this future based instead
-    return Scaffold(
-      body: AppScreenView(
-        body: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/images/logo_and_title.png',
-                package: 'genius_wallet',
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                height: 50,
-                child: LayoutBuilder(
-                  builder: (context, constraints) => TypeCreate(constraints),
-                ),
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width * 0.8,
-                height: 50,
-                child: LayoutBuilder(
-                  builder: (context, constraints) => TypeExisting(constraints),
-                ),
-              ),
-              Padding(
-                  padding: const EdgeInsets.all(40),
-                  child: ElevatedButton(
-                      onPressed: () {
-                        final dylib = DynamicLibrary.open('libnative.so');
-                        final getTemperature = dylib.lookupFunction<
-                            TemperatureFunction,
-                            TemperatureFunctionDart>('get_temperature');
-                        final valueTest = getTemperature();
-                        debugPrint('Getting valueTest From C++: $valueTest');
+    /// TODO: @David, edit this BlocBuilder according to your needs with FFI lib.
+    return BlocBuilder<AppBloc, AppState>(
+      builder: (context, state) {
+        if (state.ffiStatus == AppStatus.loading) {
+          return const CircularProgressIndicator();
+        } else if (state.ffiStatus == AppStatus.loaded) {
+          final informationFromFFI =
+              context.read<AppBloc>().state.ffiInformation;
+          return Scaffold(
+            body: AppScreenView(
+              body: SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/images/logo_and_title.png',
+                      package: 'genius_wallet',
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: 50,
+                      child: LayoutBuilder(
+                        builder: (context, constraints) =>
+                            TypeCreate(constraints),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      height: 50,
+                      child: LayoutBuilder(
+                        builder: (context, constraints) =>
+                            TypeExisting(constraints),
+                      ),
+                    ),
+                    Padding(
+                        padding: const EdgeInsets.all(40),
+                        child: ElevatedButton(
+                            onPressed: () {
+                              final dylib = DynamicLibrary.open('libnative.so');
+                              final getTemperature = dylib.lookupFunction<
+                                  TemperatureFunction,
+                                  TemperatureFunctionDart>('get_temperature');
+                              final valueTest = getTemperature();
+                              debugPrint(
+                                  'Getting valueTest From C++: $valueTest');
 
-                        // final FFIBridge ffiBridge = FFIBridge();
-                        // final valueFromC = ffiBridge.getValueFromNative();
-                        // debugPrint('Getting Value From C++: $valueFromC');
-                      },
-                      child: Text("Test C++ code in iOS!")))
-            ],
-          ),
-        ),
-      ),
+                              // final FFIBridge ffiBridge = FFIBridge();
+                              // final valueFromC = ffiBridge.getValueFromNative();
+                              // debugPrint('Getting Value From C++: ');
+                            },
+                            child: Text("Test C++ code in iOS!")))
+                  ],
+                ),
+              ),
+            ),
+          );
+        } else {
+          return const Center(
+            child: Text('Something went wrong!'),
+          );
+        }
+      },
     );
   }
 }
