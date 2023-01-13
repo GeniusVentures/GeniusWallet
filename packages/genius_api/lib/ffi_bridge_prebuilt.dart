e67d48f1dd7462122622024197d3d1cf0f88b9dc
david <davidnetizen1903@gmail.com>
2023-01-13 10:04:43 +0900

    fix on ios

--- packages/genius_api/lib/ffi_bridge_prebuilt.dart
+++ packages/genius_api/lib/ffi_bridge_prebuilt.dart
@@ -1,10 +1,14 @@
 import 'dart:ffi';
+import 'dart:io';
 
 class FFIBridgePrebuilt {
   late double Function() _libFunction;
 
   FFIBridgePrebuilt() {
-    final dylib = DynamicLibrary.open('libnative.so');
+    final dylib = Platform.isAndroid
+        ? DynamicLibrary.open('libnative.so')
+        : DynamicLibrary.process();
+
     _libFunction = dylib.lookupFunction<Double Function(), double Function()>(
         'get_temperature');
   }
