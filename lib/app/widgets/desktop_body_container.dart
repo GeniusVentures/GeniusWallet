ea6e6530959bde4deb4d7f3d91e75d816772f666
Ivan <42812006+ivan-015@users.noreply.github.com>
2023-01-16 17:12:48 -0700

    Add breakpoints to `Import Security Screen`

---
+++ lib/app/widgets/desktop_body_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:genius_wallet/theme/genius_wallet_colors.g.dart';
+
+class DesktopBodyContainer extends StatelessWidget {
+  final Widget child;
+  final EdgeInsetsGeometry padding;
+  final double? width;
+  final double? height;
+  const DesktopBodyContainer({
+    Key? key,
+    this.child = const SizedBox(),
+    this.padding = const EdgeInsets.symmetric(horizontal: 100, vertical: 80),
+    this.width = 600,
+    this.height,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: GeniusWalletColors.containerGray,
+      padding: padding,
+      width: width,
+      height: height,
+      child: child,
+    );
+  }
+}
