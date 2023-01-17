4c01d6f5f1d27ce43dde3d2e307de4155ef68252
Ivan <42812006+ivan-015@users.noreply.github.com>
2023-01-16 17:12:48 -0700

    Add breakpoints to `Import Security Screen`

--- lib/onboarding/existing_wallet/view/import_security_screen.dart
+++ lib/onboarding/existing_wallet/view/import_security_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flow_builder/flow_builder.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genius_wallet/app/utils/breakpoints.dart';
 import 'package:genius_wallet/app/widgets/app_screen_with_header.dart';
+import 'package:genius_wallet/app/widgets/desktop_body_container.dart';
 import 'package:genius_wallet/onboarding/existing_wallet/bloc/existing_wallet_bloc.dart';
 import 'package:genius_wallet/onboarding/view/address_tab_view.dart';
 import 'package:genius_wallet/onboarding/view/keystore_tab_view.dart';
@@ -69,127 +71,32 @@ class ImportSecurityScreen extends StatelessWidget {
                   title: 'Import $walletType Wallet',
                   subtitle: '',
                   bodyWidgets: [
-                    SizedBox(
-                      height: 20,
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      child: const Text('Name'),
-                    ),
-                    const SizedBox(height: 10),
-                    SizedBox(
-                      height: 50,
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      child: LayoutBuilder(
-                        builder:
-                            (BuildContext context, BoxConstraints constraints) {
-                          return TextEntryFieldWidget(
-                            logic: TextFormFieldLogic(
-                              context,
-                              controller: walletNameController,
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Please enter a wallet name';
-                                }
-                              },
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      height: 50,
-                      child: AppBar(
-                        backgroundColor: Colors.transparent,
-                        elevation: 0,
-                        bottom: const TabBar(
-                          indicatorColor: Colors.white,
-                          tabs: [
-                            Tab(text: 'Phrase'),
-                            Tab(text: 'Keystore'),
-                            Tab(text: 'Private Key'),
-                            Tab(text: 'Address'),
-                          ],
-                        ),
-                      ),
-                    ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.8,
-                      height: 400,
-                      child: TabBarView(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 25),
-                            child: PhraseTabView(
-                              controller:
-                                  tabControllers['phrase']!['pasteField']!,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10),
-                            child: KeystoreTabView(
-                              passwordController:
-                                  tabControllers['keystore']!['passwordField']!,
-                              pasteFieldController:
-                                  tabControllers['keystore']!['pasteField']!,
-                            ),
-                          ),
-                          Padding(
-                              padding: const EdgeInsets.only(top: 10),
-                              child: PrivateKeyTabView(
-                                controller: tabControllers['privatekey']![
-                                    'pasteField']!,
-                              )),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 10),
-                            child: AddressTabView(
-                              controller:
-                                  tabControllers['address']!['pasteField']!,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                  footer: SizedBox(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width * 0.8,
-                    child: LayoutBuilder(
-                      builder:
-                          (BuildContext context, BoxConstraints constraints) {
-                        return MaterialButton(
-                          onPressed: () {
-                            if (formKey.currentState!.validate()) {
-                              /// Get currently selected tab
-                              final selectedIndex =
-                                  DefaultTabController.of(context)?.index ?? 0;
-
-                              /// Get current entry to access [TextEditingControllers]
-                              final selectedEntry = tabControllers.entries
-                                  .toList()[selectedIndex];
-
-                              /// Send event with currently selected tab information
-                              context.read<ExistingWalletBloc>().add(
-                                    WalletSecurityEntered(
-                                      walletName: walletNameController.text,
-                                      walletType: walletType,
-                                      securityType: selectedEntry.key,
-                                      pasteFieldText: selectedEntry
-                                          .value['pasteField']!.text,
-                                      password: selectedEntry
-                                          .value['passwordField']?.text,
-                                    ),
-                                  );
-                            }
-                          },
-                          child: IsactiveTrue(
-                            constraints,
-                            ovrContinue: 'Import',
-                          ),
+                    Builder(builder: (context) {
+                      if (GeniusBreakpoints.useDesktopLayout(context)) {
+                        return _ImportSecurityViewDesktop(
+                          walletNameController: walletNameController,
+                          tabControllers: tabControllers,
+                          formKey: formKey,
+                          walletType: walletType,
                         );
-                      },
-                    ),
-                  ),
+                      }
+                      return _ImportSecurityBody(
+                        walletNameController: walletNameController,
+                        tabControllers: tabControllers,
+                      );
+                    }),
+                  ],
+                  footer: Builder(builder: (context) {
+                    if (GeniusBreakpoints.useDesktopLayout(context)) {
+                      return const SizedBox();
+                    }
+                    return _ImportSecurityContinueButton(
+                      formKey: formKey,
+                      tabControllers: tabControllers,
+                      walletNameController: walletNameController,
+                      walletType: walletType,
+                    );
+                  }),
                 ),
               ),
             ),
@@ -213,6 +120,192 @@ class ImportSecurityScreen extends StatelessWidget {
             },
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _ImportSecurityViewDesktop extends StatelessWidget {
+  const _ImportSecurityViewDesktop({
+    Key? key,
+    required this.walletNameController,
+    required this.tabControllers,
+    required this.formKey,
+    required this.walletType,
+  }) : super(key: key);
+
+  final TextEditingController walletNameController;
+  final Map<String, Map<String, TextEditingController>> tabControllers;
+  final GlobalKey<FormState> formKey;
+  final String walletType;
+
+  @override
+  Widget build(BuildContext context) {
+    return DesktopBodyContainer(
+      child: Column(
+        children: [
+          _ImportSecurityBody(
+            walletNameController: walletNameController,
+            tabControllers: tabControllers,
+          ),
+          _ImportSecurityContinueButton(
+            formKey: formKey,
+            tabControllers: tabControllers,
+            walletNameController: walletNameController,
+            walletType: walletType,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _ImportSecurityBody extends StatelessWidget {
+  final TextEditingController walletNameController;
+  final Map<String, Map<String, TextEditingController>> tabControllers;
+  const _ImportSecurityBody({
+    Key? key,
+    required this.walletNameController,
+    required this.tabControllers,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 20,
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: const Text('Name'),
+        ),
+        const SizedBox(height: 10),
+        SizedBox(
+          height: 50,
+          width: MediaQuery.of(context).size.width * 0.8,
+          child: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              return TextEntryFieldWidget(
+                logic: TextFormFieldLogic(
+                  context,
+                  controller: walletNameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter a wallet name';
+                    }
+                    return null;
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+        const SizedBox(height: 20),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8,
+          height: 50,
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            bottom: const TabBar(
+              indicatorColor: Colors.white,
+              tabs: [
+                Tab(text: 'Phrase'),
+                Tab(text: 'Keystore'),
+                Tab(text: 'Private Key'),
+                Tab(text: 'Address'),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(
+          width: MediaQuery.of(context).size.width * 0.8,
+          height: 400,
+          child: TabBarView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 25),
+                child: PhraseTabView(
+                  controller: tabControllers['phrase']!['pasteField']!,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: KeystoreTabView(
+                  passwordController:
+                      tabControllers['keystore']!['passwordField']!,
+                  pasteFieldController:
+                      tabControllers['keystore']!['pasteField']!,
+                ),
+              ),
+              Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: PrivateKeyTabView(
+                    controller: tabControllers['privatekey']!['pasteField']!,
+                  )),
+              Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: AddressTabView(
+                  controller: tabControllers['address']!['pasteField']!,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _ImportSecurityContinueButton extends StatelessWidget {
+  const _ImportSecurityContinueButton({
+    Key? key,
+    required this.formKey,
+    required this.tabControllers,
+    required this.walletNameController,
+    required this.walletType,
+  }) : super(key: key);
+
+  final GlobalKey<FormState> formKey;
+  final Map<String, Map<String, TextEditingController>> tabControllers;
+  final TextEditingController walletNameController;
+  final String walletType;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return MaterialButton(
+            onPressed: () {
+              if (formKey.currentState!.validate()) {
+                /// Get currently selected tab
+                final selectedIndex =
+                    DefaultTabController.of(context)?.index ?? 0;
+
+                /// Get current entry to access [TextEditingControllers]
+                final selectedEntry =
+                    tabControllers.entries.toList()[selectedIndex];
+
+                /// Send event with currently selected tab information
+                context.read<ExistingWalletBloc>().add(
+                      WalletSecurityEntered(
+                        walletName: walletNameController.text,
+                        walletType: walletType,
+                        securityType: selectedEntry.key,
+                        pasteFieldText: selectedEntry.value['pasteField']!.text,
+                        password: selectedEntry.value['passwordField']?.text,
+                      ),
+                    );
+              }
+            },
+            child: IsactiveTrue(
+              constraints,
+              ovrContinue: 'Import',
+            ),
+          );
+        },
       ),
     );
   }
