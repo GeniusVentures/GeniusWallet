580f903dc262b3fd4ca49243583a693a365413b3
xm94 <xavieralainmartinez@gmail.com>
2023-01-30 18:01:10 -0700

    enter address desktop view

--- lib/dashboard/wallets/send/view/enter_wallet_address_screen.dart
+++ lib/dashboard/wallets/send/view/enter_wallet_address_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genius_wallet/app/utils/breakpoints.dart';
+import 'package:genius_wallet/app/widgets/app_screen_desktop.dart';
 import 'package:genius_wallet/app/widgets/app_screen_view.dart';
+import 'package:genius_wallet/app/widgets/desktop_body_container.dart';
 import 'package:genius_wallet/dashboard/wallets/send/cubit/send_cubit.dart';
 import 'package:genius_wallet/widgets/components/back_button_header.g.dart';
 import 'package:genius_wallet/widgets/components/continue_button/isactive_false.g.dart';
@@ -15,93 +18,182 @@ class EnterWalletAddressScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AppScreenView(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 50,
-                  child: LayoutBuilder(
-                    builder:
-                        (BuildContext context, BoxConstraints constraints) {
-                      return BackButtonHeader(
-                        constraints,
-                        ovrTitle: 'Enter Wallet Address',
-                      );
-                    },
+      body: LayoutBuilder(builder: (context, constraints) {
+        if (GeniusBreakpoints.useDesktopLayout(context)) {
+          return const _EnterWalletAddressScreenDesktop();
+        }
+        return const _EnterWalletAddressScreenMobile();
+      }),
+    );
+  }
+}
+
+class _EnterWalletAddressScreenMobile extends StatelessWidget {
+  const _EnterWalletAddressScreenMobile({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppScreenView(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+                child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    return BackButtonHeader(
+                      constraints,
+                      ovrTitle: 'Enter Wallet Address',
+                    );
+                  },
+                ),
+              ),
+              const Spacer(),
+              Column(
+                children: [
+                  SizedBox(
+                    height: 20,
+                    width: MediaQuery.of(context).size.width,
+                    child: const Text(
+                      'Enter wallet address to send to',
+                      textAlign: TextAlign.start,
+                    ),
                   ),
-                ),
-                const Spacer(),
-                Column(
-                  children: [
-                    SizedBox(
-                      height: 20,
-                      width: MediaQuery.of(context).size.width,
-                      child: const Text(
-                        'Enter wallet address to send to',
-                        textAlign: TextAlign.start,
+                  TextEntryFieldWidget(
+                    logic: TextFormFieldLogic(
+                      context,
+                      hintText: 'Paste Address',
+                      onChanged: context.read<SendCubit>().addressUpdated,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  SizedBox(
+                    height: 62,
+                    width: MediaQuery.of(context).size.width,
+                    child: Center(
+                      child: LayoutBuilder(
+                        builder:
+                            (BuildContext context, BoxConstraints constraints) {
+                          return WalletQRScan(constraints);
+                        },
                       ),
                     ),
-                    TextEntryFieldWidget(
-                      logic: TextFormFieldLogic(
-                        context,
-                        hintText: 'Paste Address',
-                        onChanged: context.read<SendCubit>().addressUpdated,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    SizedBox(
-                      height: 62,
-                      width: MediaQuery.of(context).size.width,
-                      child: Center(
-                        child: LayoutBuilder(
-                          builder: (BuildContext context,
-                              BoxConstraints constraints) {
-                            return WalletQRScan(constraints);
-                          },
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 50,
-                    ),
-                    SizedBox(
-                      height: 50,
-                      child: LayoutBuilder(builder: (context, constraints) {
-                        return BlocBuilder<SendCubit, SendState>(
-                          builder: (context, state) {
-                            if (state.currentTransaction.toAddress.isEmpty) {
-                              return IsactiveFalse(
-                                constraints,
-                                ovrContinue: 'Proceed',
-                              );
-                            }
-                            return MaterialButton(
-                              onPressed: () {
-                                context.read<SendCubit>().addressConfirmed();
-                              },
-                              child: IsactiveTrue(
-                                constraints,
-                                ovrContinue: 'Proceed',
-                              ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  SizedBox(
+                    height: 50,
+                    child: LayoutBuilder(builder: (context, constraints) {
+                      return BlocBuilder<SendCubit, SendState>(
+                        builder: (context, state) {
+                          if (state.currentTransaction.toAddress.isEmpty) {
+                            return IsactiveFalse(
+                              constraints,
+                              ovrContinue: 'Proceed',
                             );
-                          },
-                        );
-                      }),
-                    ),
-                  ],
-                ),
-                const Spacer(flex: 3),
-              ],
-            ),
+                          }
+                          return MaterialButton(
+                            onPressed: () {
+                              context.read<SendCubit>().addressConfirmed();
+                            },
+                            child: IsactiveTrue(
+                              constraints,
+                              ovrContinue: 'Proceed',
+                            ),
+                          );
+                        },
+                      );
+                    }),
+                  ),
+                ],
+              ),
+              const Spacer(flex: 3),
+            ],
           ),
         ),
       ),
     );
+  }
+}
+
+class _EnterWalletAddressScreenDesktop extends StatelessWidget {
+  const _EnterWalletAddressScreenDesktop({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppScreenDesktop(title: 'Send Bitoin', bodyWidgets: [
+      DesktopBodyContainer(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+              width: MediaQuery.of(context).size.width,
+              child: const Text(
+                'Enter wallet address to send to',
+                textAlign: TextAlign.start,
+              ),
+            ),
+            TextEntryFieldWidget(
+              logic: TextFormFieldLogic(
+                context,
+                hintText: 'Paste Address',
+                onChanged: context.read<SendCubit>().addressUpdated,
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              height: 62,
+              width: MediaQuery.of(context).size.width,
+              child: Center(
+                child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    return WalletQRScan(constraints);
+                  },
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            SizedBox(
+              height: 50,
+              child: LayoutBuilder(builder: (context, constraints) {
+                return BlocBuilder<SendCubit, SendState>(
+                  builder: (context, state) {
+                    if (state.currentTransaction.toAddress.isEmpty) {
+                      return IsactiveFalse(
+                        constraints,
+                        ovrContinue: 'Proceed',
+                      );
+                    }
+                    return MaterialButton(
+                      onPressed: () {
+                        context.read<SendCubit>().addressConfirmed();
+                      },
+                      child: IsactiveTrue(
+                        constraints,
+                        ovrContinue: 'Proceed',
+                      ),
+                    );
+                  },
+                );
+              }),
+            ),
+          ],
+        ),
+      )
+    ]);
   }
 }
