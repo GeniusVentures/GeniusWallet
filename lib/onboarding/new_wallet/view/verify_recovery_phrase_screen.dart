7d97fe72b4c1b5d1ec886707986c666951886476
Henrique A Klein <henryaklein@gmail.com>
2023-09-09 18:29:07 -0300

    Feat: Adding list of ETH addr as possible dummy wallet addr

--- lib/onboarding/new_wallet/view/verify_recovery_phrase_screen.dart
+++ lib/onboarding/new_wallet/view/verify_recovery_phrase_screen.dart
@@ -22,6 +22,24 @@ class VerifyRecoveryPhraseScreen extends StatelessWidget {
     Key? key,
   }) : super(key: key);
 
+  static const eth_addr = [
+  "0x5b6234EcFE322f426244C856Ba94b01591570De4",
+  "0x8e8e701eBe107dFce8377EF138599F3B0cb1D1E8",
+  "0xa59854cf0c01890037283e7AC5BB29B12Fcea7cf",
+  "0x823e3270aB5a9AB98EC2f9cfA5E509e3216A858F",
+  "0x22E4F3779618DfE7642456A95B834c17f5156753",
+  "0x51BD2bdB1C965116cf96A0180D6dDb8a5607b730",
+  "0x89717B70BC33DA4ad38ECD37E1a63be9F5A22584",
+  "0xEfa9e61C00156bB2D32EdF10eD803bAb6C6FaA77",
+  "0x293815d82A0b6DE53280c4Aa5B738451c7FCd935",
+  "0x271e1238A1D56BBD79B36Be55f35b97C50C30C1A",
+  "0xF92100CAAaDBB7556bf481B6eb187AF5d4568597",
+  "0x646ef6DcB82F9BE28343084851a45790CDe62F11",
+  "0x2c70b3175b286434e3131cbAfaca7b02446744cf",
+  "0xb6D75efA4c0dA5234C590bcC00165203195A728f",
+  "0x2ba4723cBFeC5dBdB9B0b2A46080cF8a1060006E",
+  "0x02eCe6BC0f18A1B1356d6ba63F385326060e15A0",
+  ];
   @override
   Widget build(BuildContext context) {
     return BlocListener<NewWalletBloc, NewWalletState>(
@@ -36,7 +54,7 @@ class VerifyRecoveryPhraseScreen extends StatelessWidget {
                     walletName: 'dummy_wallet',
                     currencySymbol: 'GNU',
                     currencyName: 'GNUS',
-                    address: uuid.toString(),
+                    address: eth_addr[random.nextInt(eth_addr.length)].toString(),
                     balance: 0,
                     transactions: [],
                   ),
