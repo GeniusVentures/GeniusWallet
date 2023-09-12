adfdd1b2bfa601bfad9973672f019bb85f11f282
Henrique A Klein <henryaklein@gmail.com>
2023-09-12 14:53:26 -0300

    Feat: Adding query to set balance to the last query + 1

--- lib/dashboard/wallets/view/wallet_details_screen.dart
+++ lib/dashboard/wallets/view/wallet_details_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -24,91 +23,77 @@ class WalletDetailsScreenStateful extends StatefulWidget {
 }
 class WalletDetailsScreen extends State<WalletDetailsScreenStateful> {
 
-  static double _fakeBalance = 0.0;
+  late int _fakeBalance = 0;
 
   get timer => null;
+  InfluxDBClient createInfluxClient()
+  {
+    final dbClient = InfluxDBClient(
+      url: 'http://192.168.16.33:8086',
+      token: 'fdLxG5FIZnDDVOBl4gozJr5wy6mXDlBgbvOtAdzG3XfcmMzwP3_RVdHiTuK4s39pIrYoTuiJRexpQpTUTr1U1w==',
+      org: 'GNUS.AI',
+      bucket: 'WalletDB',
+      debug: true
+    );
+
+    return dbClient;
+  }
+  Future<Point> getNextVal(InfluxDBClient clientDB, String walletAddr) async {
+
+      var insertValue = Point('wallet').addTag('address', widget.curr_wallet.address).addField('balance', 1);
+      var queryService = clientDB.getQueryService();
+      var fluxQuery = '''
+          from(bucket: "${clientDB.bucket}")
+                |> range(start: -20d)
+                |> filter(fn: (r) => r["_measurement"] == "wallet" 
+                                and r.address == "$walletAddr")''';
+                                
+      var recordStream = await queryService.query(fluxQuery);
+
+      await recordStream.last.then((value)
+      {
+          insertValue.fields['balance'] += value['_value'];
+          print('\n\n Last record DB ${value['_value']} \n');
+          print('\n\n New record DB ${insertValue.fields['balance']} \n');
+      }).catchError((exception) {
+          print('\n\n Empty DB: Starting value ${insertValue.fields['balance']}\n');
+      });
+
+      return insertValue;
+  }
+  void writeNextVal(InfluxDBClient clientDB, Point nextVal) async
+  {
+      var writeApi = clientDB.getWriteService(WriteOptions().merge(
+      precision: WritePrecision.s,
+      batchSize: 100,
+      flushInterval: 5000,
+      gzip: true));
+
+      await writeApi.write(nextVal).then((value) {
+        print('Write completed 1');
+      }).catchError((exception) {
+        print('Handle write error here!');
+        print(exception);
+      });
+  }
 
   @override
   void initState() {
     super.initState();
-
     // Start a timer to refresh the value every 5 seconds
     Timer.periodic(Duration(seconds: 5), (timer) {
       // Update the value and trigger a rebuild
-
       void postWallet() async{
-        final dbClient = InfluxDBClient(
-          url: 'http://192.168.16.33:8086',
-          token: 'fdLxG5FIZnDDVOBl4gozJr5wy6mXDlBgbvOtAdzG3XfcmMzwP3_RVdHiTuK4s39pIrYoTuiJRexpQpTUTr1U1w==',
-          org: 'GNUS.AI',
-          bucket: 'WalletInfo',
-          debug: true
-        );
+        final dbClient = createInfluxClient();
 
-        var writeApi = dbClient.getWriteService(WriteOptions().merge(
-        precision: WritePrecision.s,
-        batchSize: 100,
-        flushInterval: 5000,
-        gzip: true));
+        var newBalancePoint = await getNextVal(dbClient, widget.curr_wallet.address);
+        writeNextVal(dbClient, newBalancePoint);
+        
+        _fakeBalance = newBalancePoint.fields['balance'];
 
-
-        var data = List<Point>.empty(growable: true);
-          data.add(Point('wallet')
-            .addTag('address', widget.curr_wallet.address)
-            .addField('balance', _fakeBalance));
-
-          print(
-            '\n\n-------------------------------- Write data -------------------------------\n');
-        await writeApi.write(data).then((value) {
-          print('Write completed 1');
-        }).catchError((exception) {
-          print('Handle write error here!');
-          print(exception);
-        });
-
-            // Create Query service and query
-        //var queryService = dbClient.getQueryService();
-        //var fluxQuery = '''
-        //    from(bucket: "my-bucket")
-        //          |> range(start: -20d)
-        //          |> filter(fn: (r) => r["_measurement"] == "weather" 
-        //                          and r["location"] == "Prague")''';
-//
-        //// query to stream and iterate all records
-        //print(
-        //    '\n\n---------------------------------- Query ---------------------------------\n');
-        //var recordStream = await queryService.query(fluxQuery);
-//
-        //print(
-        //    '\n\n------------------------------ Query result ------------------------------\n');
-        //await recordStream.forEach((record) {
-        //  print(
-        //      'Temperature in ${record['location']} at ${record['_time']} is ${record['_value']} °C');
-        //});
-//
-        //// delete data
-        //print(
-        //    '\n\n------------------------------- Delete data -------------------------------\n');
-        //await dbClient
-        //    .getDeleteService()
-        //    .delete(
-        //        predicate: '_measurement="weather"',
-        //        start: DateTime.parse('1970-01-01T00:00:00Z'),
-        //        stop: DateTime.now().toUtc(),
-        //        bucket: 'my-bucket',
-        //        org: 'my-org')
-        //    .catchError((e) => print(e));
-//
-        //await Future.delayed(Duration(seconds: 10));
         dbClient.close();
       }
       setState(() {
-        //print('Fake balance in $_fakeBalance');
-        //print('Fake balance in ${widget.curr_wallet.balance}');
-        //print('Fake balance in ${widget.curr_wallet.address}');
-        //_fakeBalance = widget.curr_wallet.balance;
-        _fakeBalance+=0.0001;
-        //widget.curr_wallet = widget.curr_wallet.copyWith(balance: _fakeBalance);
         postWallet();
       });
     });
@@ -173,7 +158,7 @@ class WalletDetailsScreen extends State<WalletDetailsScreenStateful> {
                           constraints,
                           ovrYourbitcoinaddress:
                               'Your ${selectedWallet.currencyName} Wallet Address',
-                          ovrQuantity: _fakeBalance.toStringAsFixed(4),
+                          ovrQuantity: (_fakeBalance.toDouble()*0.0001).toStringAsFixed(4),
                           ovrCurrency: selectedWallet.currencySymbol,
                           ovrAddressField: selectedWallet.address,
                         );
