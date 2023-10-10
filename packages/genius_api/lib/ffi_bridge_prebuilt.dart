90f413cce36defcd92158b107c5c165a28533dd6
Henrique A Klein <henryaklein@gmail.com>
2023-10-10 15:59:28 -0300

    Feat: Removing the need of wrappers. Fix protobuf dependency

--- packages/genius_api/lib/ffi_bridge_prebuilt.dart
+++ packages/genius_api/lib/ffi_bridge_prebuilt.dart
@@ -2,65 +2,30 @@ import 'dart:ffi';
 import 'dart:io';
 import 'package:genius_api/ffi/genius_api_ffi.dart';
 
-typedef MyNativeType = Pointer<Void> Function(Pointer<Uint8>, Size);
-
 class FFIBridgePrebuilt {
-  //late Pointer<T> Function<T extends NativeType>(String symbolName) _lookup;
-  //late double? Function() _libFunction;
-  //late Pointer<Void> Function(Pointer<Uint8>, int) CreateTWBytes;
+
   late NativeLibrary wallet_lib;
-        late double? Function() _libFunction;
 
   FFIBridgePrebuilt() {
     DynamicLibrary? dylib;
     if (Platform.isAndroid) {
-      //dylib = DynamicLibrary.process();
       dylib = DynamicLibrary.open('libGeniusWallet.so');
     } else if (Platform.isIOS) {
       dylib = DynamicLibrary.process();
     } else if (Platform.environment.containsKey('FLUTTER_TEST')) {
-      dylib = DynamicLibrary.open('test/${Platform.operatingSystem}/.build/libwalletWrapper.so');
+      dylib = DynamicLibrary.open('test/${Platform.operatingSystem}/.build/libGeniusWallet.so');
     } else {
       dylib = null;
     }
     if (dylib != null) {
       wallet_lib = NativeLibrary(dylib);
-      _libFunction = dylib.lookupFunction<Double Function(), double Function()>(
-        'get_temperature');
-      //_lookup = dylib.lookup;
-      //_libFunction = dylib.lookupFunction<Double Function(), double Function()>(
-      //    'TWDataCreateWithBytes');
-      //CreateTWBytes = dylib
-      //    .lookup<NativeFunction<Pointer<Void> Function(Pointer<Uint8>, Size)>>(
-      //        'TWDataCreateWithBytes')
-      //    .asFunction<Pointer<Void> Function(Pointer<Uint8>, int)>();
+
     } else {
-      //CreateTWBytes = null as Pointer<Void> Function(Pointer<Uint8>, int);
     }
   }
-//  late final _TWDataCreateWithBytesPtr = _lookup<
-//      NativeFunction<
-//          Pointer<Pointer<Void>> Function(
-//              Pointer<Uint8>, Size)>>('TWDataCreateWithBytes');
-//  late final _TWDataCreateWithBytes = _TWDataCreateWithBytesPtr.asFunction<
-//      Pointer<Pointer<Void>> Function(Pointer<Uint8>, int)>();
-//
-//  Pointer<Pointer<Void>> TWDataCreateWithBytes(
-//    Pointer<Uint8> bytes,
-//    int size,
-//  ) {
-//    return _TWDataCreateWithBytes(
-//      bytes,
-//      size,
-//    );
-double? getValueFromNative() => _libFunction();
-  //Pointer<Void> TWDataCreateWithBytes(
-  //  Pointer<Uint8> bytes,
-  //  int size,
-  //) {
-  //  return CreateTWBytes(
-  //    bytes,
-  //    size,
-  //  );
-  //}
+  double? getValueFromNative() 
+  {
+    return 12.0;
+  }
+
 }
