5a68d9da1bc6f6a25cb9bc1d96c898b229b2b82f
Henrique A Klein <henryaklein@gmail.com>
2023-10-11 10:38:27 -0300

    Feat: Adding default path of thirdparty

--- packages/genius_api/lib/ffi_bridge_prebuilt.dart
+++ packages/genius_api/lib/ffi_bridge_prebuilt.dart
@@ -4,16 +4,19 @@ import 'package:genius_api/ffi/genius_api_ffi.dart';
 
 class FFIBridgePrebuilt {
 
+  static const String _libName = 'GeniusWallet';
   late NativeLibrary wallet_lib;
 
   FFIBridgePrebuilt() {
     DynamicLibrary? dylib;
-    if (Platform.isAndroid) {
-      dylib = DynamicLibrary.open('libGeniusWallet.so');
-    } else if (Platform.isIOS) {
+    if (Platform.environment.containsKey('FLUTTER_TEST')) {
+      dylib = DynamicLibrary.open('test/${Platform.operatingSystem}/.build/lib$_libName.so');
+    } else if (Platform.isAndroid || Platform.isLinux) {
+      dylib = DynamicLibrary.open('lib$_libName.so');
+    } else if (Platform.isMacOS || Platform.isIOS) {
       dylib = DynamicLibrary.process();
-    } else if (Platform.environment.containsKey('FLUTTER_TEST')) {
-      dylib = DynamicLibrary.open('test/${Platform.operatingSystem}/.build/libGeniusWallet.so');
+    } else if (Platform.isWindows) {
+      dylib = DynamicLibrary.open('$_libName.dll');
     } else {
       dylib = null;
     }
