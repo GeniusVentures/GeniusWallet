acce632791e9df80afc663a9f091b8e442d062bd
Matthew Homan <48568237+matthewhoman@users.noreply.github.com>
2024-04-26 13:39:08 -0400

    Wallets, app bar, header fixes (#49)
    * update the wallet backup screen to match latest figma
    
    * Update wallet_agreement_custom.dart
    
    * pin screen finished
    
    * Update existing_wallet_bloc.dart
    
    * Wallets Screen updates

--- lib/app/widgets/overlay/destinations.dart
+++ lib/app/widgets/overlay/destinations.dart
@@ -4,53 +4,27 @@ import 'package:genius_wallet/app/widgets/overlay/genius_destination.dart';
 
 class GeniusTabDestinations {
   static final destinations = [
-    GeniusDestination(
-      destination: '/dashboard',
-      icon: Image.asset(
-        'assets/images/navdashboardbuttoncustom.png',
-        package: 'genius_wallet',
-      ),
-      label: const Text('Dashboard'),
-      selectedIcon: Image.asset(
-        'assets/images/navdashboardbuttonactive.png',
-        package: 'genius_wallet',
-      ),
-    ),
-    GeniusDestination(
+    const GeniusDestination(
+        destination: '/dashboard',
+        icon: Icon(Icons.dashboard),
+        label: Text('Dashboard'),
+        selectedIcon: Icon(Icons.dashboard)),
+    const GeniusDestination(
       destination: '/wallets',
-      icon: Image.asset(
-        'assets/images/navwalletbuttoncustom.png',
-        package: 'genius_wallet',
-      ),
-      label: const Text('Wallets'),
-      selectedIcon: Image.asset(
-        'assets/images/navwalletbuttonactive.png',
-        package: 'genius_wallet',
-      ),
+      icon: Icon(Icons.account_balance_wallet),
+      label: Text('Wallets'),
+      selectedIcon: Icon(Icons.account_balance_wallet),
     ),
-    GeniusDestination(
-      destination: '/transactions',
-      icon: Image.asset(
-        'assets/images/navtimerbuttoncustom.png',
-        package: 'genius_wallet',
-      ),
-      label: const Text('Transactions'),
-      selectedIcon: Image.asset(
-        'assets/images/navtimerbuttonactive.png',
-        package: 'genius_wallet',
-      ),
-    ),
-    GeniusDestination(
+    const GeniusDestination(
+        destination: '/transactions',
+        icon: Icon(Icons.timer_rounded),
+        label: Text('Transactions'),
+        selectedIcon: Icon(Icons.timer_rounded)),
+    const GeniusDestination(
       destination: '/trade',
-      icon: SvgPicture.asset(
-        'assets/images/tradedesktopiconunselected.svg',
-        package: 'genius_wallet',
-      ),
-      label: const Text('Trade'),
-      selectedIcon: SvgPicture.asset(
-        'assets/images/tradedesktopiconselected.svg',
-        package: 'genius_wallet',
-      ),
+      icon: Icon(Icons.candlestick_chart),
+      label: Text('Trade'),
+      selectedIcon: Icon(Icons.candlestick_chart),
     ),
     GeniusDestination(
       destination: '/markets',
--- lib/widgets/components/wallet_module.g.dart
+++ lib/widgets/components/wallet_module.g.dart
@@ -1,12 +1,12 @@
-// *********************************************************************************
-// PARABEAC-GENERATED CODE. DO NOT MODIFY.
-//
-// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
-// *********************************************************************************
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:genius_wallet/theme/genius_wallet_colors.g.dart';
+import 'package:genius_wallet/theme/genius_wallet_consts.dart';
+import 'package:genius_wallet/theme/genius_wallet_font_size.dart';
+import 'package:genius_wallet/theme/genius_wallet_text.dart';
 
 class WalletModule extends StatefulWidget {
   final BoxConstraints constraints;
@@ -40,178 +40,170 @@ class _WalletModule extends State<WalletModule> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(),
+        padding:
+            const EdgeInsets.only(top: 40, bottom: 40, left: 20, right: 20),
+        height: widget.constraints.maxHeight * 1.0,
+        width: widget.constraints.maxWidth * 1.0,
+        decoration: const BoxDecoration(
+          color: GeniusWalletColors.deepBlueCardColor,
+          borderRadius: BorderRadius.all(
+              Radius.circular(GeniusWalletConsts.borderRadiusCard)),
+        ),
         child: Stack(children: [
-          Positioned(
-            left: 0,
-            width: widget.constraints.maxWidth * 1.0,
-            top: 0,
-            height: widget.constraints.maxHeight * 1.0,
-            child: Stack(children: [
-              Positioned(
-                left: 0,
-                width: widget.constraints.maxWidth * 1.0,
-                top: 0,
-                height: widget.constraints.maxHeight * 1.0,
-                child: Container(
-                  height: widget.constraints.maxHeight * 1.0,
-                  width: widget.constraints.maxWidth * 1.0,
-                  decoration: BoxDecoration(
-                    color: Color(0xff1d2024),
-                    borderRadius: BorderRadius.all(Radius.circular(2.0)),
-                  ),
+          Center(
+              child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(shape: BoxShape.circle, boxShadow: [
+                    BoxShadow(
+                      color:
+                          GeniusWalletColors.lightGreenPrimary.withOpacity(.5),
+                      spreadRadius: 2,
+                      blurRadius: 80,
+                    )
+                  ]))),
+          Row(children: [
+            widget.ovrCoinImage ??
+                Image.asset(
+                  'assets/images/coinimage.png',
+                  package: 'genius_wallet',
+                  fit: BoxFit.fill,
                 ),
+            const SizedBox(width: 16),
+            AutoSizeText(
+              widget.ovrCoinName ?? 'Bitcoin',
+              style: const TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: GeniusWalletFontSize.medium,
+                fontWeight: FontWeight.w400,
+                letterSpacing: 0.25,
+                color: Colors.white,
               ),
-              Positioned(
-                left: widget.constraints.maxWidth * 0.064,
-                width: widget.constraints.maxWidth * 0.113,
-                top: widget.constraints.maxHeight * 0.094,
-                height: widget.constraints.maxHeight * 0.118,
-                child: widget.ovrCoinImage ??
-                    Image.asset(
-                      'assets/images/coinimage.png',
-                      package: 'genius_wallet',
-                      height:
-                          widget.constraints.maxHeight * 0.11784511784511785,
-                      width: widget.constraints.maxWidth * 0.11254019292604502,
-                      fit: BoxFit.fill,
-                    ),
+              textAlign: TextAlign.left,
+            ),
+            const Expanded(
+                child: Text(
+              GeniusWalletText.walletCardHeader,
+              textAlign: TextAlign.end,
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 12.0,
+                fontWeight: FontWeight.w500,
+                letterSpacing: 0.2571428716182709,
+                color: Colors.white,
               ),
-              Positioned(
-                left: widget.constraints.maxWidth * 0.514,
-                width: widget.constraints.maxWidth * 0.421,
-                top: widget.constraints.maxHeight * 0.155,
-                height: widget.constraints.maxHeight * 0.111,
-                child: Container(
-                    height: widget.constraints.maxHeight * 0.1111111111111111,
-                    width: widget.constraints.maxWidth * 0.4212218649517685,
-                    child: AutoSizeText(
-                      widget.ovrWalletBalance ?? '0.221746',
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 28.0,
-                        fontWeight: FontWeight.w300,
-                        letterSpacing: 0.28000009059906006,
-                        color: Colors.white,
-                      ),
-                      textAlign: TextAlign.right,
-                    )),
-              ),
-              Positioned(
-                left: widget.constraints.maxWidth * 0.219,
-                width: widget.constraints.maxWidth * 0.283,
-                top: widget.constraints.maxHeight * 0.114,
-                height: widget.constraints.maxHeight * 0.077,
-                child: Container(
-                    height: widget.constraints.maxHeight * 0.07744107744107744,
-                    width: widget.constraints.maxWidth * 0.2829581993569132,
-                    child: AutoSizeText(
-                      widget.ovrCoinName ?? 'Bitcoin',
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.w300,
-                        letterSpacing: 0.25,
-                        color: Colors.white,
-                      ),
-                      textAlign: TextAlign.left,
-                    )),
-              ),
-              Positioned(
-                left: 20.0,
-                right: 20.0,
-                bottom: 22.0,
-                height: 72.0,
-                child: Container(
-                    decoration: BoxDecoration(),
-                    child: Stack(children: [
-                      Positioned(
-                        left: 0,
-                        width: widget.constraints.maxWidth * 0.871,
-                        top: 0,
-                        height: 72.0,
-                        child: Container(
-                          height: 72.0,
-                          width:
-                              widget.constraints.maxWidth * 0.8713826366559485,
-                          decoration: BoxDecoration(
-                            color: Color(0xff2a2b31),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(2.0)),
-                          ),
+            ))
+          ]),
+          Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Row(children: [
+                Expanded(
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                      AutoSizeText(
+                        widget.ovrWalletBalance ?? '0.221746',
+                        style: const TextStyle(
+                          fontFamily: 'Roboto',
+                          fontSize: GeniusWalletFontSize.sectionHeaderLarge,
+                          fontWeight: FontWeight.w700,
+                          letterSpacing: 0.28000009059906006,
+                          color: Colors.white,
                         ),
                       ),
-                      Positioned(
-                        left: widget.constraints.maxWidth * 0.061,
-                        width: widget.constraints.maxWidth * 0.338,
-                        top: 13.0,
-                        height: 12.0,
-                        child: Container(
-                            height: 12.0,
-                            width: widget.constraints.maxWidth *
-                                0.33762057877813506,
-                            child: AutoSizeText(
-                              widget.ovrTimestamp ?? '16:01, 12 dec 2018',
-                              style: TextStyle(
-                                fontFamily: 'Roboto',
-                                fontSize: 10.0,
-                                fontWeight: FontWeight.w400,
-                                letterSpacing: 0.25,
-                                color: Colors.white,
-                              ),
-                              textAlign: TextAlign.left,
-                            )),
-                      ),
-                      Positioned(
-                        left: widget.constraints.maxWidth * 0.582,
-                        width: widget.constraints.maxWidth * 0.244,
-                        top: 13.0,
-                        height: 14.0,
-                        child: Container(
-                            height: 14.0,
-                            width: widget.constraints.maxWidth *
-                                0.24437299035369775,
-                            child: AutoSizeText(
-                              widget.ovrLastTransactionValue ?? '+1.045 BTC',
-                              style: TextStyle(
-                                fontFamily: 'Roboto',
-                                fontSize: 12.0,
-                                fontWeight: FontWeight.w400,
-                                letterSpacing: 0.0,
-                                color: Colors.white,
-                              ),
-                              textAlign: TextAlign.left,
-                            )),
-                      ),
-                      Positioned(
-                        left: widget.constraints.maxWidth * 0.039,
-                        width: widget.constraints.maxWidth * 0.788,
-                        top: 45.0,
-                        height: 14.0,
-                        child: Container(
-                            height: 14.0,
-                            width: widget.constraints.maxWidth *
-                                0.7877813504823151,
-                            child: AutoSizeText(
-                              widget.ovrLastTransactionID ??
-                                  '1PRj85hu9RXPZTzxtko9stfs6nRo1vyrQB',
-                              style: TextStyle(
-                                fontFamily: 'Roboto',
-                                fontSize: 12.0,
-                                fontWeight: FontWeight.w400,
-                                letterSpacing: 0.30000001192092896,
-                                color: Color(0xff42434b),
-                              ),
-                              textAlign: TextAlign.center,
-                            )),
-                      ),
+                      const SizedBox(width: 5),
+                      Padding(
+                          padding: const EdgeInsets.only(top: 12),
+                          child: AutoSizeText(
+                            widget.ovrCoinSymbol ?? 'Btc',
+                            style: const TextStyle(
+                              fontFamily: 'Roboto',
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 0.2571428716182709,
+                              color: GeniusWalletColors.gray500,
+                            ),
+                          ))
                     ])),
-              ),
-              Positioned(
-                left: widget.constraints.maxWidth * 0.064,
-                width: widget.constraints.maxWidth * 0.871,
-                top: widget.constraints.maxHeight * 0.293,
-                height: widget.constraints.maxHeight * 0.296,
+              ])),
+          Positioned(
+            left: 20.0,
+            right: 20.0,
+            bottom: 22.0,
+            height: 72.0,
+            child: Container(
+                decoration: BoxDecoration(),
+                child: Stack(children: [
+                  Positioned(
+                    left: widget.constraints.maxWidth * 0.061,
+                    width: widget.constraints.maxWidth * 0.338,
+                    top: 13.0,
+                    height: 12.0,
+                    child: Container(
+                        height: 12.0,
+                        width:
+                            widget.constraints.maxWidth * 0.33762057877813506,
+                        child: AutoSizeText(
+                          widget.ovrTimestamp ?? '16:01, 12 dec 2018',
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 10.0,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 0.25,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.left,
+                        )),
+                  ),
+                  Positioned(
+                    left: widget.constraints.maxWidth * 0.582,
+                    width: widget.constraints.maxWidth * 0.244,
+                    top: 13.0,
+                    height: 14.0,
+                    child: Container(
+                        height: 14.0,
+                        width:
+                            widget.constraints.maxWidth * 0.24437299035369775,
+                        child: AutoSizeText(
+                          widget.ovrLastTransactionValue ?? '+1.045 BTC',
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 0.0,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.left,
+                        )),
+                  ),
+                  Positioned(
+                    left: widget.constraints.maxWidth * 0.039,
+                    width: widget.constraints.maxWidth * 0.788,
+                    top: 55.0,
+                    height: 14.0,
+                    child: Container(
+                        height: 14.0,
+                        width: widget.constraints.maxWidth * 0.7877813504823151,
+                        child: AutoSizeText(
+                          widget.ovrLastTransactionID ??
+                              '1PRj85hu9RXPZTzxtko9stfs6nRo1vyrQB',
+                          style: TextStyle(
+                            fontFamily: 'Roboto',
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 0.30000001192092896,
+                            color: Colors.white,
+                          ),
+                          textAlign: TextAlign.center,
+                        )),
+                  ),
+                ])),
+          ),
+          Positioned(
+            left: widget.constraints.maxWidth * 0.064,
+            width: widget.constraints.maxWidth * 0.871,
+            top: widget.constraints.maxHeight * 0.293,
+            height: widget.constraints.maxHeight * 0.296,
+            child: Container(
                 child: widget.ovrTrendLine ??
                     SvgPicture.asset(
                       'assets/images/trendline.svg',
@@ -219,29 +211,7 @@ class _WalletModule extends State<WalletModule> {
                       height: widget.constraints.maxHeight * 0.2962962962962963,
                       width: widget.constraints.maxWidth * 0.8713826366559485,
                       fit: BoxFit.fill,
-                    ),
-              ),
-              Positioned(
-                left: widget.constraints.maxWidth * 0.727,
-                width: widget.constraints.maxWidth * 0.209,
-                top: widget.constraints.maxHeight * 0.094,
-                height: widget.constraints.maxHeight * 0.047,
-                child: Container(
-                    height: widget.constraints.maxHeight * 0.04713804713804714,
-                    width: widget.constraints.maxWidth * 0.2090032154340836,
-                    child: AutoSizeText(
-                      widget.ovrCoinSymbol ?? 'Btc',
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        fontSize: 12.0,
-                        fontWeight: FontWeight.w700,
-                        letterSpacing: 0.2571428716182709,
-                        color: Color(0xff3a3c43),
-                      ),
-                      textAlign: TextAlign.right,
                     )),
-              ),
-            ]),
           ),
         ]));
   }
