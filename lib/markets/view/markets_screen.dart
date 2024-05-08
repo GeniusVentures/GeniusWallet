cf002730355a4bba808ba9fb3568deba0e1b4f22
Matthew Homan <48568237+matthewhoman@users.noreply.github.com>
2024-05-08 10:09:32 -0400

    Update Markets Screen

--- lib/markets/view/markets_screen.dart
+++ lib/markets/view/markets_screen.dart
@@ -6,7 +6,6 @@ import 'package:genius_wallet/app/widgets/app_screen_view.dart';
 import 'package:genius_wallet/markets/bloc/markets_cubit.dart';
 import 'package:genius_wallet/markets/bloc/markets_state.dart';
 import 'package:genius_wallet/theme/genius_wallet_colors.g.dart';
-import 'package:genius_wallet/widgets/components/back_button_header.g.dart';
 import 'package:genius_wallet/widgets/components/market_graph.g.dart';
 import 'package:go_router/go_router.dart';
 
@@ -16,6 +15,7 @@ class MarketsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: GeniusWalletColors.deepBlueTertiary,
       body: AppScreenView(
         body: BlocProvider(
           create: (context) =>
@@ -33,28 +33,19 @@ class _CalculatorView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
+      padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Column(
         children: [
-          SizedBox(
-            height: 50,
-            child: LayoutBuilder(
-              builder: (context, constraints) {
-                return BackButtonHeader(
-                  constraints,
-                  ovrTitle: '',
-                );
-              },
-            ),
-          ),
-          const SizedBox(height: 30),
           Row(
             crossAxisAlignment: CrossAxisAlignment.end,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               const Text(
                 'Markets',
-                style: TextStyle(fontSize: 24),
+                style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: .3),
               ),
               GestureDetector(
                 onTap: () {
@@ -65,12 +56,12 @@ class _CalculatorView extends StatelessWidget {
                   style: Theme.of(context)
                       .textTheme
                       .titleMedium!
-                      .copyWith(color: GeniusWalletColors.darkGreen),
+                      .copyWith(color: GeniusWalletColors.lightGreenSecondary),
                 ),
               ),
             ],
           ),
-          const SizedBox(height: 30),
+          const SizedBox(height: 50),
           const MarketsList(),
         ],
       ),
@@ -91,15 +82,16 @@ class MarketsList extends StatelessWidget {
             return Padding(
               padding: const EdgeInsets.only(bottom: 20.0),
               child: SizedBox(
-                height: 200,
+                height: 225,
                 child: LayoutBuilder(
                   builder: (BuildContext context, BoxConstraints constraints) {
                     //TODO: Get volume
                     return MarketGraph(
                       constraints,
-                      ovrPricePerCoin: '${e.price} ${e.priceCurrency}',
+                      ovrPriceCurrency: e.priceCurrency,
+                      ovrPricePerCoin: e.price,
                       ovrCoinToFiat: '${e.symbol}/${e.priceCurrency}',
-                      ovrVolume: '',
+                      ovrVolume: null,
                     );
                   },
                 ),
