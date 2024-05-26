5607af6615575b53a395f6662109b144794e64d4
Henrique A Klein <henryaklein@gmail.com>
2024-05-26 18:41:32 -0300

    WIP: Link SDK to GeniusWallet

--- cmake/CommonCompilerOptions.cmake
+++ cmake/CommonCompilerOptions.cmake
@@ -11,3 +11,29 @@ if (NOT DEFINED THIRDPARTY_DIR)
     message( FATAL_ERROR "Cannot find thirdparty directory required to build" )
   endif()
 endif()
+
+# --------------------------------------------------------
+# Set config of SuperGenius project
+if (NOT DEFINED SUPERGENIUS_SRC_DIR)
+  if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/../../SuperGenius/Readme.md")
+    message("Setting default SuperGenius directory")
+    set(SUPERGENIUS_SRC_DIR "${CMAKE_CURRENT_LIST_DIR}/../../SuperGenius" CACHE STRING "Default SuperGenius Library")
+    ## get absolute path
+    #cmake_path(SET SUPERGENIUS_SRC_DIR NORMALIZE "${SUPERGENIUS_SRC_DIR}")
+  else()
+    message( FATAL_ERROR "Cannot find SuperGenius directory required to build" )
+  endif()
+endif()
+
+# --------------------------------------------------------
+# Set config of SuperGenius project
+if (NOT DEFINED GENIUSSDK_SRC_DIR)
+  if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/../../GeniusSDK/Readme.MD")
+    message("Setting default SuperGenius directory")
+    set(GENIUSSDK_SRC_DIR "${CMAKE_CURRENT_LIST_DIR}/../../GeniusSDK" CACHE STRING "Default SuperGenius Library")
+    ## get absolute path
+    #cmake_path(SET GENIUSSDK_SRC_DIR NORMALIZE "${GENIUSSDK_SRC_DIR}")
+  else()
+    message( FATAL_ERROR "Cannot find SuperGenius directory required to build" )
+  endif()
+endif()
