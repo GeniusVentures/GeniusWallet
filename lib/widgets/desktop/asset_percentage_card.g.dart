12be71ffbf0a060e1969c9ad32c7b8c67ded190b
Matthew Homan <48568237+matthewhoman@users.noreply.github.com>
2024-05-30 17:17:56 -0400

    Update asset_percentage_card.g.dart

--- lib/widgets/desktop/asset_percentage_card.g.dart
+++ lib/widgets/desktop/asset_percentage_card.g.dart
@@ -1,7 +1,10 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genius_wallet/dashboard/wallets/cubit/wallet_details_cubit.dart';
 import 'package:genius_wallet/theme/genius_wallet_colors.g.dart';
 import 'package:genius_wallet/theme/genius_wallet_consts.dart';
+import 'package:go_router/go_router.dart';
 
 enum Crypto {
   btc('BTC'),
@@ -135,14 +138,24 @@ class _AssetPercentageCard extends State<AssetPercentageCard> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              AutoSizeText(widget.ovrReceiveBitcoin ?? 'Receive Bitcoin'),
+              FilledButton(
+                  onPressed: null,
+                  // TODO: wire this receive
+                  // onPressed: () => context.push('/receive',
+                  //     extra: context.read<WalletDetailsCubit>()),
+                  style: const ButtonStyle(
+                      backgroundColor:
+                          MaterialStatePropertyAll(GeniusWalletColors.gray500)),
+                  child: Text(widget.ovrReceiveBitcoin ?? 'Receive Bitcoin',
+                      style: const TextStyle(color: Colors.white))),
               FilledButton(
                   style: const ButtonStyle(
                       padding: MaterialStatePropertyAll(
                           EdgeInsets.only(left: 50, right: 50)),
                       backgroundColor: MaterialStatePropertyAll(
                           GeniusWalletColors.lightGreenPrimary)),
-                  onPressed: null,
+                  // TODO: wire this send
+                  onPressed: (null),
                   child: Text(
                     widget.ovrsend ?? 'Send',
                     style: const TextStyle(
