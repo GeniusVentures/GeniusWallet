d3e9c3e0782e381f91653f8204c97ddd3eefb087
Matthew Homan <48568237+matthewhoman@users.noreply.github.com>
2024-05-30 17:20:58 -0400

    wallet details, mobile vs desktop pathing

--- lib/dashboard/wallets/view/wallet_details_screen.dart
+++ lib/dashboard/wallets/view/wallet_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:genius_wallet/app/utils/breakpoints.dart';
 import 'package:genius_wallet/app/utils/wallet_utils.dart';
 import 'package:genius_wallet/app/widgets/app_screen_view.dart';
 import 'package:genius_wallet/dashboard/wallets/cubit/wallet_details_cubit.dart';
@@ -20,6 +21,95 @@ class WalletDetailsScreen extends StatelessWidget {
             child: Text('Error'),
           );
         }
+        if (GeniusBreakpoints.useDesktopLayout(context)) {
+          return const Desktop();
+        }
+        return const Mobile();
+      },
+    );
+  }
+}
+
+class Desktop extends StatelessWidget {
+  const Desktop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<WalletDetailsCubit, WalletDetailsState>(
+      builder: (context, state) {
+        final selectedWallet = state.selectedWallet!;
+        return Scaffold(
+          backgroundColor: GeniusWalletColors.deepBlueTertiary,
+          appBar: AppBar(
+            backgroundColor: GeniusWalletColors.deepBlueTertiary,
+          ),
+          body: AppScreenView(
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 18),
+                  SizedBox(
+                    height: 40,
+                    child: LayoutBuilder(
+                      builder:
+                          (BuildContext context, BoxConstraints constraints) {
+                        return WalletToggle(
+                          constraints,
+                          ovrCoinName: selectedWallet.currencyName,
+                          ovrWalletName: selectedWallet.walletName,
+                          ovrShape: WalletUtils.currencySymbolToImage(
+                            selectedWallet.currencySymbol,
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 18),
+                  SizedBox(
+                    height: 240,
+                    child: LayoutBuilder(
+                      builder:
+                          (BuildContext context, BoxConstraints constraints) {
+                        return WalletInformation(
+                          constraints,
+                          ovrYourbitcoinaddress:
+                              'Your ${selectedWallet.currencyName} Wallet Address',
+                          ovrQuantity: selectedWallet.balance.toString(),
+                          ovrCurrency: selectedWallet.currencySymbol,
+                          ovrAddressField: selectedWallet.address,
+                        );
+                      },
+                    ),
+                  ),
+                  const SizedBox(height: 18),
+                  SizedBox(
+                    height: 370,
+                    child: LayoutBuilder(
+                      builder:
+                          (BuildContext context, BoxConstraints constraints) {
+                        return Transactions(constraints);
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class Mobile extends StatelessWidget {
+  const Mobile({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<WalletDetailsCubit, WalletDetailsState>(
+      builder: (context, state) {
         final selectedWallet = state.selectedWallet!;
         return Scaffold(
           backgroundColor: GeniusWalletColors.deepBlueTertiary,
