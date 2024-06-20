1e2b7546e0b494285358cad7a7004d969be34064
Matthew Homan <48568237+matthewhoman@users.noreply.github.com>
2024-06-20 16:43:48 -0400

    Transaction Information Desktop (#66)
    * fix cubit for popup
    
    * Update transactions_screen.dart

--- lib/dashboard/transactions/view/transaction_information_screen.dart
+++ lib/dashboard/transactions/view/transaction_information_screen.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:genius_api/genius_api.dart';
-import 'package:genius_wallet/app/widgets/app_screen_view.dart';
-import 'package:genius_wallet/widgets/components/back_button_header.g.dart';
 import 'package:genius_wallet/widgets/components/custom/detailed_transaction_status_custom.dart';
 import 'package:genius_wallet/widgets/components/historic_transaction_detail_view.g.dart';
 
@@ -14,63 +12,40 @@ class TransactionInformationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: AppScreenView(
-        body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 50,
-                child: LayoutBuilder(
-                  builder: (BuildContext context, BoxConstraints constraints) {
-                    return BackButtonHeader(
-                      constraints,
-                      ovrTitle: 'Transaction Information',
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(height: 30),
-              SizedBox(
-                height: 40,
-                width: 121,
-                child: LayoutBuilder(
-                  builder: (BuildContext context, BoxConstraints constraints) {
-                    return DetailedTransactionStatusCustom();
-                  },
-                ),
-              ),
-              const SizedBox(height: 30),
-              SizedBox(
-                height: 500,
-                child: LayoutBuilder(
-                  builder: (BuildContext context, BoxConstraints constraints) {
-                    return HistoricTransactionDetailView(
-                      constraints,
-                      ovrTransactionAmount: transaction.amount,
-                      ovrCryptoType: transaction.coinSymbol,
-                      ovrReceiverWalletID: transaction.toAddress,
-                      ovrTransactionID: transaction.hash,
-                      ovrTransactionFee: transaction.fees,
-                      ovrTransactionType: transaction.transactionDirection ==
-                              TransactionDirection.received
-                          ? 'Incoming'
-                          : 'Outgoing',
-                      ovrTransactionTypeArrow: transaction
-                                  .transactionDirection ==
-                              TransactionDirection.received
-                          ? Image.asset('assets/images/green_arrow_right.png')
-                          : Image.asset('assets/images/red_arrow_left.png'),
-                      ovrTransactionTimestamp: transaction.timeStamp,
-                    );
-                  },
-                ),
-              ),
-            ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              alignment: Alignment.center,
+              child: DetailedTransactionStatusCustom()),
+          const SizedBox(height: 30),
+          SizedBox(
+            height: 500,
+            child: LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+                return HistoricTransactionDetailView(
+                  constraints,
+                  ovrTransactionAmount: transaction.amount,
+                  ovrCryptoType: transaction.coinSymbol,
+                  ovrReceiverWalletID: transaction.toAddress,
+                  ovrTransactionID: transaction.hash,
+                  ovrTransactionFee: transaction.fees,
+                  ovrTransactionType: transaction.transactionDirection ==
+                          TransactionDirection.received
+                      ? 'Incoming'
+                      : 'Outgoing',
+                  ovrTransactionTypeArrow: transaction.transactionDirection ==
+                          TransactionDirection.received
+                      ? Image.asset('assets/images/green_arrow_right.png')
+                      : Image.asset('assets/images/red_arrow_left.png'),
+                  ovrTransactionTimestamp: transaction.timeStamp,
+                );
+              },
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
