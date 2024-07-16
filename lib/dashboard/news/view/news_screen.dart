6e02a3235322f03499174cd5f27e950c98bbdfab
Matthew Homan <48568237+matthewhoman@users.noreply.github.com>
2024-07-16 02:02:16 -0400

    news work, update service with image, dashboard layout, responsive

--- lib/dashboard/news/view/news_screen.dart
+++ lib/dashboard/news/view/news_screen.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genius_api/genius_api.dart';
@@ -6,18 +7,18 @@ import 'package:genius_wallet/app/screens/loading_screen.dart';
 import 'package:genius_wallet/app/utils/breakpoints.dart';
 import 'package:genius_wallet/app/widgets/app_screen_view.dart';
 import 'package:genius_wallet/app/widgets/desktop_container.dart';
+import 'package:genius_wallet/app/widgets/responsive_dashboard_layout.dart';
 import 'package:genius_wallet/app/widgets/responsive_grid.dart';
 import 'package:genius_wallet/dashboard/news/view/bloc/news_cubit.dart';
 import 'package:genius_wallet/dashboard/news/view/bloc/news_state.dart';
 import 'package:genius_wallet/theme/genius_wallet_colors.g.dart';
-import 'package:genius_wallet/theme/genius_wallet_consts.dart';
 
 class NewsScreen extends StatelessWidget {
   const NewsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (GeniusBreakpoints.useDesktopLayout(context)) {
+    if (!GeniusBreakpoints.isNativeApp(context)) {
       return BlocProvider(
         create: (context) =>
             NewsCubit(api: context.read<GeniusApi>())..loadNews(),
@@ -45,10 +46,30 @@ class Desktop extends StatelessWidget {
           if (state.newsLoadingStatus == NewsStatus.loading) {
             return const LoadingScreen();
           }
-          return Wrap(
-              spacing: GeniusWalletConsts.itemSpacing,
-              runSpacing: GeniusWalletConsts.itemSpacing,
-              children: [for (var news in state.news) NewsCard(news: news)]);
+          return Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Expanded(
+                flex: 3,
+                child: Column(children: [
+                  Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                    Expanded(
+                        flex: 1,
+                        child: NewsCard(news: state.news[0], height: 400)),
+                    Expanded(
+                        flex: 3,
+                        child: NewsCard(news: state.news[1], height: 400))
+                  ]),
+                  Row(children: [
+                    Expanded(
+                        flex: 3,
+                        child: NewsCard(news: state.news[2], height: 400)),
+                    Expanded(
+                        flex: 1,
+                        child: NewsCard(news: state.news[3], height: 400))
+                  ])
+                ])),
+            Expanded(
+                flex: 1, child: NewsCard(news: state.news[4], height: 800)),
+          ]);
         }));
   }
 }
@@ -67,7 +88,7 @@ class Mobile extends StatelessWidget {
               if (state.newsLoadingStatus == NewsStatus.loading) {
                 return const LoadingScreen();
               }
-              return ResponsiveGrid(children: [
+              return Column(children: [
                 for (var news in state.news)
                   SizedBox(
                     height: 350,
@@ -88,10 +109,51 @@ class Mobile extends StatelessWidget {
 
 class NewsCard extends StatelessWidget {
   final News news;
-  const NewsCard({Key? key, required this.news}) : super(key: key);
+  final double height;
+  const NewsCard({Key? key, required this.news, required this.height})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(child: Text(news.date ?? ''));
+    return SizedBox(
+        height: height,
+        child: CustomScrollView(slivers: [
+          SliverFillRemaining(
+              hasScrollBody: false,
+              child: Flex(direction: Axis.horizontal, children: [
+                Expanded(
+                    child: Card(
+                        color: GeniusWalletColors.deepBlueCardColor,
+                        child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              if (news.imgSrc != null)
+                                Image.network(news.imgSrc ?? ""),
+                              Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 20, right: 20, top: 24, bottom: 24),
+                                  child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          news.date ?? '',
+                                          style: const TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w700),
+                                        ),
+                                        const SizedBox(height: 8),
+                                        Text(news.headline ?? '',
+                                            style:
+                                                const TextStyle(fontSize: 20)),
+                                        const SizedBox(height: 8),
+                                        Text(news.body ?? '',
+                                            style:
+                                                const TextStyle(fontSize: 14))
+                                      ])),
+                            ])))
+              ]))
+        ]));
   }
 }
