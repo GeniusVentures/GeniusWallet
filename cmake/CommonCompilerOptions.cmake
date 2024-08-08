64409811248cd0aba29e987f0a590c1984193c59
Henrique A Klein <henryaklein@gmail.com>
2024-08-08 17:11:11 -0300

    Chore: Adding back sdk and supergenius src dir but changed to check directory only

--- cmake/CommonCompilerOptions.cmake
+++ cmake/CommonCompilerOptions.cmake
@@ -6,7 +6,7 @@ set(CMAKE_CXX_EXTENSIONS OFF)
 # --------------------------------------------------------
 # define third party directory
 if (NOT DEFINED THIRDPARTY_DIR)
-  if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/../../thirdparty/README.md")
+  if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/../../thirdparty")
     message("Setting default third party directory")
     set(THIRDPARTY_DIR "${CMAKE_CURRENT_LIST_DIR}/../../thirdparty")
     ## get absolute path
@@ -18,26 +18,26 @@ endif()
 
 # --------------------------------------------------------
 # Set config of SuperGenius project
-#if (NOT DEFINED SUPERGENIUS_SRC_DIR)
-#  if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/../../SuperGenius/Readme.md")
-#    message("Setting default SuperGenius directory")
-#    set(SUPERGENIUS_SRC_DIR "${CMAKE_CURRENT_LIST_DIR}/../../SuperGenius" CACHE STRING "Default SuperGenius Library")
-#    ## get absolute path
-#    #cmake_path(SET SUPERGENIUS_SRC_DIR NORMALIZE "${SUPERGENIUS_SRC_DIR}")
-#  else()
-#    message( FATAL_ERROR "Cannot find SuperGenius directory required to build" )
-#  endif()
-#endif()
+if (NOT DEFINED SUPERGENIUS_SRC_DIR)
+  if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/../../SuperGenius")
+    message("Setting default SuperGenius directory")
+    set(SUPERGENIUS_SRC_DIR "${CMAKE_CURRENT_LIST_DIR}/../../SuperGenius" CACHE STRING "Default SuperGenius Library")
+    ## get absolute path
+    #cmake_path(SET SUPERGENIUS_SRC_DIR NORMALIZE "${SUPERGENIUS_SRC_DIR}")
+  else()
+    message( FATAL_ERROR "Cannot find SuperGenius directory required to build" )
+  endif()
+endif()
 
 # --------------------------------------------------------
 # Set config of SuperGenius project
-#if (NOT DEFINED GENIUSSDK_SRC_DIR)
-#  if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/../../GeniusSDK/Readme.MD")
-#    message("Setting default GeniusSDK directory")
-#    set(GENIUSSDK_SRC_DIR "${CMAKE_CURRENT_LIST_DIR}/../../GeniusSDK" CACHE STRING "Default SuperGenius Library")
-#    ## get absolute path
-#    #cmake_path(SET GENIUSSDK_SRC_DIR NORMALIZE "${GENIUSSDK_SRC_DIR}")
-#  else()
-#    message( FATAL_ERROR "Cannot find GeniusSDK directory required to build" )
-#  endif()
-#endif()
+if (NOT DEFINED GENIUSSDK_SRC_DIR)
+  if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/../../GeniusSDK")
+    message("Setting default GeniusSDK directory")
+    set(GENIUSSDK_SRC_DIR "${CMAKE_CURRENT_LIST_DIR}/../../GeniusSDK" CACHE STRING "Default GeniusSDK Library")
+    ## get absolute path
+    #cmake_path(SET GENIUSSDK_SRC_DIR NORMALIZE "${GENIUSSDK_SRC_DIR}")
+  else()
+    message( FATAL_ERROR "Cannot find GeniusSDK directory required to build" )
+  endif()
+endif()
