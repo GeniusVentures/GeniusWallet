fc4083621b480133b53b9fa51d213a7f41f2759c
Eduardo Menges Mattje <eduardomattje@gmail.com>
2024-11-14 17:02:27 -0300

    Regenerated freeze files

--- packages/genius_api/lib/models/transaction.g.dart
+++ packages/genius_api/lib/models/transaction.g.dart
@@ -6,15 +6,30 @@ part of 'transaction.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
+_$TransferRecipientsImpl _$$TransferRecipientsImplFromJson(
+        Map<String, dynamic> json) =>
+    _$TransferRecipientsImpl(
+      toAddr: json['toAddr'] as String,
+      amount: json['amount'] as String,
+    );
+
+Map<String, dynamic> _$$TransferRecipientsImplToJson(
+        _$TransferRecipientsImpl instance) =>
+    <String, dynamic>{
+      'toAddr': instance.toAddr,
+      'amount': instance.amount,
+    };
+
 _$TransactionImpl _$$TransactionImplFromJson(Map<String, dynamic> json) =>
     _$TransactionImpl(
       hash: json['hash'] as String,
       fromAddress: json['fromAddress'] as String,
-      toAddress: json['toAddress'] as String,
+      recipients: (json['recipients'] as List<dynamic>)
+          .map((e) => TransferRecipients.fromJson(e as Map<String, dynamic>))
+          .toList(),
       timeStamp: DateTime.parse(json['timeStamp'] as String),
       transactionDirection: $enumDecode(
           _$TransactionDirectionEnumMap, json['transactionDirection']),
-      amount: json['amount'] as String,
       fees: json['fees'] as String,
       coinSymbol: json['coinSymbol'] as String,
       transactionStatus:
@@ -26,11 +41,10 @@ Map<String, dynamic> _$$TransactionImplToJson(_$TransactionImpl instance) =>
     <String, dynamic>{
       'hash': instance.hash,
       'fromAddress': instance.fromAddress,
-      'toAddress': instance.toAddress,
+      'recipients': instance.recipients,
       'timeStamp': instance.timeStamp.toIso8601String(),
       'transactionDirection':
           _$TransactionDirectionEnumMap[instance.transactionDirection]!,
-      'amount': instance.amount,
       'fees': instance.fees,
       'coinSymbol': instance.coinSymbol,
       'transactionStatus':
