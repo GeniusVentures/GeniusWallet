dde702c61e7ec4c2b70fce50b9f49dc20bedc9bb
itsafuu <sariru@gmail.com>
2025-04-01 18:51:36 -0400

    Use set to avoid duplicates in transaction list

--- packages/genius_api/lib/controllers/transactions_controller.dart
+++ packages/genius_api/lib/controllers/transactions_controller.dart
@@ -3,31 +3,29 @@ import 'package:genius_api/models/transaction.dart';
 import 'package:rxdart/rxdart.dart';
 
 class TransactionsController {
-  final List<Transaction> _transactions = [];
-
+  final Set<Transaction> _transactions = {};
   final _controller = BehaviorSubject<List<Transaction>>();
-
   Stream<List<Transaction>> get stream => _controller.stream;
-
+  
   TransactionsController() {
     _controller.add([]);
   }
-
+  
   void addTransaction(Transaction transaction) {
-    _transactions.add(transaction);
-    _controller.add(List.unmodifiable(_transactions.reversed.toList()));
+    _transactions.add(transaction); // Will use freezed's equality implementation
+    _controller.add(List.unmodifiable(_transactions.toList().reversed.toList()));
   }
-
+  
   void addTransactions(List<Transaction> newTxs) {
     _transactions.addAll(newTxs);
-    _controller.add(List.unmodifiable(_transactions.reversed.toList()));
+    _controller.add(List.unmodifiable(_transactions.toList().reversed.toList()));
   }
-
+  
   void clear() {
     _transactions.clear();
     _controller.add([]);
   }
-
+  
   void dispose() {
     _controller.close();
   }
