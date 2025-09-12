b8300cbe714fbfd4115b8fba30fb559c46f97093
Ammar Yasir Khan <128680867+Ammaryasirkhan@users.noreply.github.com>
2025-09-12 17:59:34 +0500

    added resposnsiveness fixes

--- lib/banaxa/banaxa_orders_history.dart
+++ lib/banaxa/banaxa_orders_history.dart
@@ -115,12 +115,12 @@ class _OrdersPageState extends State<OrdersPage> {
               ),
         title: const Text("My Orders"),
         actions: [
-          IconButton(
-            icon: const Icon(Icons.app_registration_rounded),
-            onPressed: () {
-              context.push('/kyc');
-            },
-          ),
+          // IconButton(
+          //   icon: const Icon(Icons.app_registration_rounded),
+          //   onPressed: () {
+          //     context.push('/kyc');
+          //   },
+          // ),
           IconButton(
             icon: const Icon(Icons.refresh),
             onPressed: () {
