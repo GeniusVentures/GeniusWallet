01ff130e12e58d2f3cf273f13f75ed5cb30b1aff
TechUp24 <developer.techup24@gmail.com>
2025-09-20 14:24:00 +0500

    Techup Banxa and Github Issues

--- macos/native/build/cmake_install.cmake
+++ macos/native/build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/ammaryasir/development/Projects/genius/GeniusWallet/macos/native
+# Install script for directory: /Users/ammaryasir/Downloads/GeniusWallet/macos/native
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,7 +40,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/ammaryasir/development/Projects/genius/GeniusWallet/macos/native/build/install_local_manifest.txt"
+  file(WRITE "/Users/ammaryasir/Downloads/GeniusWallet/macos/native/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -56,6 +56,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/Users/ammaryasir/development/Projects/genius/GeniusWallet/macos/native/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/Users/ammaryasir/Downloads/GeniusWallet/macos/native/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
