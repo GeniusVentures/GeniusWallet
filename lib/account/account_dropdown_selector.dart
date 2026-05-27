bcce8b35db655de4dabb526d6ce69175e91a34ca
Eduardo Menges Mattje <eduardomattje@gmail.com>
2026-05-27 10:09:28 -0300

    Using `MenuAnchor` instead

--- lib/account/account_dropdown_selector.dart
+++ lib/account/account_dropdown_selector.dart
@@ -27,8 +27,7 @@ class AccountDropdownSelector extends StatefulWidget {
   });
 
   @override
-  State<AccountDropdownSelector> createState() =>
-      _AccountDropdownSelectorState();
+  State<AccountDropdownSelector> createState() => _AccountDropdownSelectorState();
 }
 
 class _AccountDropdownSelectorState extends State<AccountDropdownSelector> {
@@ -90,13 +89,10 @@ class _AccountDropdownSelectorState extends State<AccountDropdownSelector> {
 
       // If the deleted wallet was the selected one, select another.
       if (wallet.address == selectedWallet?.address) {
-        final remainingWallets = appBloc.state.wallets
-            .where((w) => w.address != wallet.address)
-            .toList();
+        final remainingWallets =
+            appBloc.state.wallets.where((w) => w.address != wallet.address).toList();
         setState(() {
-          selectedWallet = remainingWallets.isNotEmpty
-              ? remainingWallets.first
-              : null;
+          selectedWallet = remainingWallets.isNotEmpty ? remainingWallets.first : null;
         });
       }
     }
@@ -109,8 +105,8 @@ class _AccountDropdownSelectorState extends State<AccountDropdownSelector> {
       context: context,
       title: "Your Accounts",
       child: ListView.separated(
-        itemBuilder: (context, i) => _buildDrawerRow(
-            wallets[i], wallets[i].walletName == selectedWallet?.walletName),
+        itemBuilder: (context, i) =>
+            _buildDrawerRow(wallets[i], wallets[i].walletName == selectedWallet?.walletName),
         itemCount: wallets.length,
         separatorBuilder: (context, index) => SizedBox(height: 8.0),
       ),
@@ -118,8 +114,7 @@ class _AccountDropdownSelectorState extends State<AccountDropdownSelector> {
         padding: const EdgeInsets.all(16.0),
         child: FilledButton.icon(
           style: FilledButton.styleFrom(
-              textStyle:
-                  const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              textStyle: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
               iconSize: 28,
               padding: EdgeInsets.all(16.0)),
           onPressed: () => context.push('/landing_screen', extra: true),
@@ -143,11 +138,9 @@ class _AccountDropdownSelectorState extends State<AccountDropdownSelector> {
   ) {
     final isWatched = wallet.walletType == WalletType.tracking;
 
-    final textColor =
-        isSelected ? GeniusWalletColors.deepBlueTertiary : Colors.white;
+    final textColor = isSelected ? GeniusWalletColors.deepBlueTertiary : Colors.white;
 
-    final subColor =
-        isSelected ? GeniusWalletColors.deepBlueTertiary : Colors.grey;
+    final subColor = isSelected ? GeniusWalletColors.deepBlueTertiary : Colors.grey;
 
     return ListTile(
       selected: isSelected,
@@ -230,15 +223,25 @@ class _AccountDropdownSelectorState extends State<AccountDropdownSelector> {
       ),
       trailing: wallet.address.isEmpty
           ? null
-          : PopupMenuButton<_WalletAction>(
-              icon: const Icon(
-                Icons.more_vert,
-                size: 20,
-                color: Colors.white70,
+          : MenuAnchor(
+              builder: (context, controller, child) => IconButton(
+                icon: Icon(
+                  Icons.more_vert,
+                  size: 20,
+                  color: textColor,
+                ),
+                onPressed: () {
+                  if (controller.isOpen) {
+                    controller.close();
+                  } else {
+                    controller.open();
+                  }
+                },
               ),
-              onSelected: (action) {
-                switch (action) {
-                  case _WalletAction.copy:
+              menuChildren: [
+                MenuItemButton(
+                  leadingIcon: const Icon(Icons.copy, size: 20),
+                  onPressed: () {
                     Clipboard.setData(
                       ClipboardData(text: wallet.address),
                     );
@@ -249,35 +252,22 @@ class _AccountDropdownSelectorState extends State<AccountDropdownSelector> {
                       'Address copied to clipboard',
                       duration: const Duration(seconds: 1),
                     );
-                  case _WalletAction.delete:
-                    _confirmDeleteWallet(context, wallet);
-                }
-              },
-              itemBuilder: (context) => [
-                const PopupMenuItem(
-                  value: _WalletAction.copy,
-                  child: ListTile(
-                    leading: Icon(Icons.copy, size: 20),
-                    title: Text('Copy address'),
-                  ),
+                  },
+                  child: const Text('Copy address'),
                 ),
                 if (wallet.walletType != WalletType.sgnus)
-                  const PopupMenuItem(
-                    value: _WalletAction.delete,
-                    child: ListTile(
-                      leading: Icon(Icons.delete_outline,
-                          size: 20, color: Colors.redAccent),
-                      title: Text('Delete wallet',
-                          style: TextStyle(color: Colors.redAccent)),
-                    ),
+                  MenuItemButton(
+                    leadingIcon:
+                        const Icon(Icons.delete_outline, size: 20, color: Colors.redAccent),
+                    onPressed: () => _confirmDeleteWallet(context, wallet),
+                    child: const Text('Delete wallet', style: TextStyle(color: Colors.redAccent)),
                   ),
               ],
             ),
     );
   }
 
-  Widget _buildAvatar(Wallet wallet,
-      {required bool isSelected, required double size}) {
+  Widget _buildAvatar(Wallet wallet, {required bool isSelected, required double size}) {
     final isWatched = wallet.walletType == WalletType.tracking;
     return CircleAvatar(
       radius: size / 2 - 2,
@@ -340,8 +330,7 @@ class _AccountDropdownSelectorState extends State<AccountDropdownSelector> {
                       child: Text(
                         selectedWallet!.walletType == WalletType.sgnus
                             ? 'Super Genius'
-                            : WalletUtils.getAddressForDisplay(
-                                selectedWallet!.address),
+                            : WalletUtils.getAddressForDisplay(selectedWallet!.address),
                         style: const TextStyle(fontSize: 14),
                         overflow: TextOverflow.ellipsis,
                       ),
@@ -357,5 +346,3 @@ class _AccountDropdownSelectorState extends State<AccountDropdownSelector> {
     );
   }
 }
-
-enum _WalletAction { copy, delete }
--- lib/screens/pin_screen.dart
+++ lib/screens/pin_screen.dart
@@ -93,8 +93,7 @@ class _PinViewDesktop extends StatelessWidget {
               SizedBox(
                 height: 50,
                 child: LayoutBuilder(
-                  builder:
-                      (BuildContext context, BoxConstraints constraints) {
+                  builder: (BuildContext context, BoxConstraints constraints) {
                     return BlocBuilder<PinCubit, PinState>(
                       builder: (context, state) {
                         if (state.pinFullness == PinFullness.completed) {
@@ -150,8 +149,7 @@ class _PinViewMobile extends StatelessWidget {
                         width: 200,
                         height: 40,
                         child: LayoutBuilder(
-                          builder: (BuildContext context,
-                              BoxConstraints constraints) {
+                          builder: (BuildContext context, BoxConstraints constraints) {
                             return IncorrectPin(constraints);
                           },
                         ),
@@ -204,8 +202,7 @@ class _PinViewMobile extends StatelessWidget {
                     length: GeniusWalletConsts.pinCount,
                     onChanged: (passcode) {},
                     readOnly: true,
-                    pinController:
-                        context.watch<PinCubit>().state.pinController,
+                    pinController: context.watch<PinCubit>().state.pinController,
                     onCompleted: onCompleted,
                   ),
                 ),
