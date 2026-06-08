4a1bf5bf0b742509d0fd62531a5a9003c4cb1076
Eduardo Menges Mattje <eduardomattje@gmail.com>
2026-06-08 15:40:12 -0300

    Deleted more unused files

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:genius_wallet/bloc/app_bloc.dart';
-import 'package:genius_wallet/components/app_screen_view.dart';
 import 'package:genius_wallet/components/loading.dart';
 import 'package:genius_wallet/theme/genius_wallet_colors.dart';
 import 'package:go_router/go_router.dart';
@@ -23,21 +22,17 @@ class Splash extends StatelessWidget {
       },
       child: Scaffold(
         backgroundColor: GeniusWalletColors.deepBlue,
-        body: AppScreenView(
-          body: SizedBox(
-            width: MediaQuery.sizeOf(context).width,
-            height: MediaQuery.sizeOf(context).height,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/images/logo_and_title.png',
-                  package: 'genius_wallet',
-                ),
-                const SizedBox(height: 24),
-                const Loading(),
-              ],
-            ),
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/images/logo_and_title.png',
+                package: 'genius_wallet',
+              ),
+              const SizedBox(height: 24),
+              const Loading(),
+            ],
           ),
         ),
       ),
